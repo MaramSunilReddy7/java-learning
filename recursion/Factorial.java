3486dbd618c0562dbccb8fa1b9d6fabfeea34dcc
Maram Sunil Reddy <67134688+MaramSunilReddy7@users.noreply.github.com>
2022-08-23 17:50:07 +0530

    Add files via upload

---
+++ recursion/Factorial.java
@@ -0,0 +1,20 @@
+package recurion1;
+
+public class Factorial {
+	public static int fact(int n) {
+		if(n==0) {
+			return 1;
+			
+		}
+		int smallFact = fact(n-1);
+		return n*smallFact;
+	}
+
+	public static void main(String[] args) {
+		int n = 3;
+		int ans = fact(n);
+		System.out.println(ans);
+
+	}
+
+}
